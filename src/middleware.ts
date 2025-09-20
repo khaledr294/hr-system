ebd73dcc60167949822cd465f0f2495fe082b89f
Saed System <system@saed.com>
2025-09-20 03:43:55 +0300

    Protect root path in middleware and handle login redirect

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,26 @@
 ﻿import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
 
-export default withAuth({
-  pages: {
-    signIn: '/auth/login'
+export default withAuth(
+  function middleware(req) {
+    const { nextauth, nextUrl } = req;
+    const token = nextauth?.token;
+    const path = nextUrl.pathname;
+
+    if (token && path === '/auth/login') {
+      return NextResponse.redirect(new URL('/workers', req.url));
+    }
+    return NextResponse.next();
+  },
+  {
+    pages: {
+      signIn: '/auth/login'
+    }
   }
-});
+);
 
 export const config = { matcher: [
+  '/',
   '/workers/:path*',
   '/clients/:path*',
   '/contracts/:path*',
